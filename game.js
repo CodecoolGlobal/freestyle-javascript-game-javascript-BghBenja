aae07215ea0d30ce224a38ec4db74044017cc19e
Kelemen Angi <angela.kelemen.nemeth@gmail.com>
2022-02-08 14:28:48 +0100

    Add base board

--- game.js
+++ game.js
@@ -1,24 +1,77 @@
-initGame();
+const game = {
+    init: function () {
+        console.log("Valami")
+        this.drawBoard();
+        let myNavbar = document.querySelector(".navbar");
+        console.log(myNavbar);
 
-function initGame() {
+        // TODO: do the rest of the game setup here (eg. add event listeners)
+        this.initRightClick();
+        this.initLeftClick();
+    },
 
-    // Your game can start here, but define separate functions, don't write everything in here :)
+    drawBoard: function () {
 
-}
+        const rows = 10
+        const cols = 10
 
+        let gameField = document.querySelector(".game-field");
+        console.log(gameField);
+        this.setGameFieldSize(gameField, rows, cols);
+        let cellIndex = 0
+        for (let row = 0; row < rows; row++) {
+            const rowElement = this.addRow(gameField);
+            for (let col = 0; col < cols; col++) {
+                this.addCell(rowElement, row, col);
+                cellIndex++;
+            }
+        }
+    },
 
-let board = ""
+    setGameFieldSize: function (gameField, rows, cols) {
+        gameField.style.width = (gameField.dataset.cellWidth * rows) + 'px';
+        gameField.style.height = (gameField.dataset.cellHeight * cols) + 'px';
+    },
+    addRow: function (gameField) {
+        gameField.insertAdjacentHTML(
+            'beforeend',
+            '<div class="row"></div>'
+        );
+        return gameField.lastElementChild;
+    },
+    addCell: function (rowElement, row, col) {
+        rowElement.insertAdjacentHTML(
+            'beforeend',
+            `<div class="field"
+                        data-row="${row}"
+                        data-col="${col}"></div>`);
+    },
 
-for (i = 0; i < 6; i++) {
-    for (i = 0; i < 6; i++) {
-        board += "<div class='grid-item' id=''></div>"
-    }
-}
+};
 
-document.querySelector(".board").innerHTML = board
+game.init();
 
-const level1 = [[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 1, 1, 1, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0]]
-
-// function createBoard() {
+// initGame();
+//
+// function initGame() {
+//
+//     // Your game can start here, but define separate functions, don't write everything in here :)
+//
 // }
+//
+//
+// let board = ""
+//
+// for (i = 0; i < 6; i++) {
+//     for (i = 0; i < 6; i++) {
+//         board += "<div class='grid-item' id=''></div>"
+//     }
+// }
+//
+// document.querySelector(".board").innerHTML = board
+//
+// const level1 = [[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 1, 1, 1, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0]]
+//
+// // function createBoard() {
+// // }
 
