c65288f364749d00878411785a4b858a5c8c6dce
oliviakovacs <kovacs.olivia.s@gmail.com>
2022-02-08 14:04:46 +0100

    add directories

--- game.js
+++ game.js
@@ -5,3 +5,20 @@ function initGame() {
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }
+
+
+let board = ""
+
+for (i = 0; i < 6; i++) {
+    for (i = 0; i < 6; i++) {
+        board += "<div class='grid-item' id=''></div>"
+    }
+}
+
+document.querySelector(".board").innerHTML = board
+
+const level1 = [[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 1, 1, 1, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0]]
+
+// function createBoard() {
+// }
+
