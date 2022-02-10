a0ad51a66436405a1222214a4277caf9006af331
Kelemen Angi <angela.kelemen.nemeth@gmail.com>
2022-02-10 13:21:37 +0100

    Add highscore

--- game.js
+++ game.js
@@ -1,32 +1,47 @@
-var LEVEL1 = [[0,1,1,1,1,1,1,1,1,1,1,1,1,1],[0,1,2,2,2,2,2,2,2,2,2,2,2,1],[1,1,2,2,2,2,2,2,2,2,2,2,2,1],[1,2,2,2,2,2,2,2,2,2,2,2,2,1],[1,2,2,2,5,2,2,4,2,2,3,2,2,1],[1,2,2,2,2,2,2,2,2,2,2,2,2,1],[1,2,2,2,2,2,2,2,2,2,2,1,1,1],[1,2,2,2,2,2,2,2,2,2,2,1,0,0],[1,1,1,1,1,1,1,1,1,1,1,1,0,0]]
+const LEVEL1 = [[0,1,1,1,1,1,1,1,1,1,1,1,1,1],[0,1,2,2,2,2,2,2,2,2,2,2,2,1],[1,1,2,2,2,2,2,2,2,2,2,2,2,1],[1,2,2,2,2,2,2,2,2,2,2,2,2,1],[1,2,2,2,5,2,2,4,2,2,3,2,2,1],[1,2,2,2,2,2,2,2,2,2,2,2,2,1],[1,2,2,2,2,2,2,2,2,2,2,1,1,1],[1,2,2,2,2,2,2,2,2,2,2,1,0,0],[1,1,1,1,1,1,1,1,1,1,1,1,0,0]]
 
-var LEVEL2 = [[1,1,1,1,1,1,1,1,1],[1,2,2,2,2,2,2,2,1],[1,2,2,2,1,1,2,2,1],[1,1,2,2,2,2,2,2,1],[0,1,2,1,1,1,1,1,1],[0,1,4,1,0,0,0,0,0],[0,1,2,1,1,0,0,0,0],[1,1,2,3,1,1,1,0,0],[1,2,5,4,2,5,1,0,0],[1,1,1,1,1,1,1,0,0]]
+const LEVEL2 = [[1,1,1,1,1,1,1,1,1],[1,2,2,2,2,2,2,2,1],[1,2,2,2,1,1,2,2,1],[1,1,2,2,2,2,2,2,1],[0,1,2,1,1,1,1,1,1],[0,1,4,1,0,0,0,0,0],[0,1,2,1,1,0,0,0,0],[1,1,2,3,1,1,1,0,0],[1,2,5,4,2,5,1,0,0],[1,1,1,1,1,1,1,0,0]]
 
-LEVEL3 = [[1,1,1,1,1,1,0,0,1,1,1,0],[1,5,5,2,2,1,0,1,1,3,1,1],[1,5,5,2,2,1,1,1,2,2,2,1],[1,5,5,2,2,2,2,2,4,4,2,1],[1,5,5,2,2,1,2,1,2,4,2,1],[1,5,5,1,1,1,2,1,2,4,2,1],[1,1,1,1,2,4,2,1,4,2,2,1],[0,0,0,1,2,2,4,1,2,4,2,1],[0,0,0,1,2,4,2,2,4,2,2,1],[0,0,0,1,2,2,1,1,2,2,2,1],[0,0,0,1,1,1,1,1,1,1,1,1]]
+const LEVEL3 = [[1,1,1,1,1,1,0,0,1,1,1,0],[1,5,5,2,2,1,0,1,1,3,1,1],[1,5,5,2,2,1,1,1,2,2,2,1],[1,5,5,2,2,2,2,2,4,4,2,1],[1,5,5,2,2,1,2,1,2,4,2,1],[1,5,5,1,1,1,2,1,2,4,2,1],[1,1,1,1,2,4,2,1,4,2,2,1],[0,0,0,1,2,2,4,1,2,4,2,1],[0,0,0,1,2,4,2,2,4,2,2,1],[0,0,0,1,2,2,1,1,2,2,2,1],[0,0,0,1,1,1,1,1,1,1,1,1]]
 
-let level1Targets = [{row:"4", col:"4"}]
-let level2Targets = [{row:"9", col:"2"}]
-let level3Targets = [{row:"1", col:"1"}, {row:"1", col:"2"}, {row:"2", col:"1"}, {row:"2", col:"2"}, {row:"3", col:"1"}, {row:"3", col:"2"}, {row:"4", col:"1"}, {row:"4", col:"2"}, {row:"5", col:"1"}, {row:"5", col:"2"}]
+const level1Targets = [{row:"4", col:"4"}]
+const level2Targets = [{row:"8", col:"2"}, {row:"8", col:"5"}]
+const level3Targets = [{row:"1", col:"1"}, {row:"1", col:"2"}, {row:"2", col:"1"}, {row:"2", col:"2"}, {row:"3", col:"1"}, {row:"3", col:"2"}, {row:"4", col:"1"}, {row:"4", col:"2"}, {row:"5", col:"1"}, {row:"5", col:"2"}]
 
+let actualLevel = 1
 
+function createLocalstorageElements() {
+    if (!localStorage.getItem("highscore1")) {
+        console.log(localStorage.getItem("highscore1"))
+        localStorage.setItem("highscore1", "Doesn't exist, yet!")
+    };
+    if (!localStorage.getItem("highscore2")) {
+        localStorage.setItem("highscore2", "Doesn't exist, yet!")
+    };
+    if (!localStorage.getItem("highscore3")) {
+        localStorage.setItem("highscore3", "Doesn't exist, yet!")
+    }
+}
+
+createLocalstorageElements();
 
 
 const game = {
-    init: function () {
-        this.drawBoard();
-        let myNavbar = document.querySelector(".navbar");
+    init: function (currentLevel) {
+        let level = currentLevel;
+        this.drawBoard(level);
     },
 
-    drawBoard: function () {
-        const rows = LEVEL1.length
-        const cols = LEVEL1[0].length
+    drawBoard: function (level) {
+        const rows = level.length
+        const cols = level[0].length
 
         let gameField = document.querySelector(".game-field");
         this.setGameFieldSize(gameField, rows, cols);
         for (let row = 0; row < rows; row++) {
             const rowElement = this.addRow(gameField);
             for (let col = 0; col < cols; col++) {
-                switch (LEVEL1[row][col]) {
+                switch (level[row][col]) {
                     case 0:
                         this.addCell(rowElement, row, col, "void");
                         break;
@@ -72,7 +87,10 @@ const game = {
 
 };
 
-game.init();
+
+let highscore1 = localStorage.getItem("highscore1");
+document.querySelector(".highscore").innerHTML = `Your highscore on this level: ${highscore1}`;
+game.init(LEVEL1);
 
 function movement(player, newCell, newBoxCell) {
     if (newCell.className == 'field' || newCell.className == 'target') {
@@ -131,51 +149,117 @@ myBody.addEventListener('keydown', function(event) {
             movement(player, newCell4, newBoxCell4)
             break;
     }
-    targetChecker()
+    console.log(`actual level ${actualLevel}`)
+    if (actualLevel == 1) {
+        targetChecker(level1Targets)
+    } else if (actualLevel == 2) {
+        targetChecker(level2Targets)
+    } else {
+        targetChecker(level3Targets)
+    }
+
 });
 
 
 let myRestartButton = document.querySelector(".button-restart")
 myRestartButton.addEventListener("click", function(event) {
-    location.reload()
+    if (actualLevel == 1) {
+        let myBoard = document.querySelector(".game-field")
+        myBoard.innerHTML = ""
+        game.init(LEVEL1);
+    } else if (actualLevel == 2) {
+        let myBoard = document.querySelector(".game-field")
+        myBoard.innerHTML = ""
+        game.init(LEVEL2);
+    } else {
+        let myBoard = document.querySelector(".game-field")
+        myBoard.innerHTML = ""
+        game.init(LEVEL3);
+    }
 } );
 
 
 
-function targetChecker (){
+function targetChecker (targets){
     let counter = 0
-    for(let element of level1Targets){
+    for(let element of targets){
         let row = element.row
         let col = element.col
         let currentCell = document.querySelector(`[data-row = '${row}' ][data-col = '${col}' ]`);
-        console.log(currentCell)
         if (currentCell.className == "field"){
-            console.log("bent van")
             currentCell.className = "target"
         }
         if (currentCell.className == "box"){
             counter++
         }
     }
-    if (counter == level1Targets.length){
-        winner()
+    if (counter == targets.length){
+        setLocalStorage();
+        setTimeout(() => finishingLevel(), 500)
     }
 }
 
-function winner () {
-        let counter = 0;
-        for(let element of level1Targets){
-        let row = element.row
-        let col = element.col
-        let currentCell = document.querySelector(`[data-row = '${row}' ][data-col = '${col}' ]`);
-        console.log(currentCell)
-        if (currentCell.className == "box") {
-            counter++
+function finishingLevel() {
+    if (confirm("Proceed to the next level")) {
+        if (actualLevel == 1) {
+            let allTimeHighscore1 = localStorage.getItem("highscore1");
+            if (keypressCount < Number(allTimeHighscore1) || allTimeHighscore1 == "Doesn't exist, yet!") {
+                localStorage.setItem("highscore1", keypressCount)
+            }
+            document.querySelector(".highscore").innerHTML = `Highscore on this level: ${keypressCount}`
+            actualLevel++
+            let myBoard = document.querySelector(".game-field")
+            myBoard.innerHTML = ""
+            keypressCount = 0
+            let myKeypressCounter = document.querySelector('.keypressCounter')
+            myKeypressCounter.innerHTML = `Number of your moves: ${keypressCount}`;
+            let highscore2 = localStorage.getItem("highscore2");
+            document.querySelector(".highscore").innerHTML = `Highscore on this level: ${highscore2}`
+            game.init(LEVEL2)
+        } else if (actualLevel == 2) {
+            actualLevel++
+            let myBoard = document.querySelector(".game-field")
+            myBoard.innerHTML = ""
+            keypressCount = 0
+            let myKeypressCounter = document.querySelector('.keypressCounter')
+            myKeypressCounter.innerHTML = `Number of your moves: ${keypressCount}`;
+            let highscore3 = localStorage.getItem("highscore3");
+            document.querySelector(".highscore").innerHTML = `Highscore on this level: ${highscore3}`
+            game.init(LEVEL3)
+        } else if (actualLevel == 3) {
+            console.log("no more proceed")
+        } else {
+            alert("You won!")
         }
-        if (counter == level1Targets.length){
-        console.log("nyertel")
-        alert("YESSSSS")
-        }
+    } else {
+        location.reload();
     }
 };
+
+function setLocalStorage() {
+    if (actualLevel == 1) {
+        let allTimeHighscore1 = localStorage.getItem("highscore1");
+        if (keypressCount < Number(allTimeHighscore1) || allTimeHighscore1 == "Doesn't exist, yet!") {
+            localStorage.setItem("highscore1", keypressCount);
+            setTimeout(() => alert("New Highscore!"), 500)
+        }
+        document.querySelector(".highscore").innerHTML = `Highscore on this level: ${keypressCount}`
+
+    } else if (actualLevel == 2) {
+        let allTimeHighscore2 = localStorage.getItem("highscore2");
+        if (keypressCount < Number(allTimeHighscore2) || allTimeHighscore2 == "Doesn't exist, yet!") {
+            localStorage.setItem("highscore2", keypressCount);
+            setTimeout(() => alert("New Highscore!"), 500);
+        }
+        document.querySelector(".highscore").innerHTML = `Highscore on this level: ${keypressCount}`
+    } else if (actualLevel == 3) {
+        let allTimeHighscore3 = localStorage.getItem("highscore3");
+        if (keypressCount < Number(allTimeHighscore3) || allTimeHighscore3 == "Doesn't exist, yet!") {
+            localStorage.setItem("highscore3", keypressCount);
+            setTimeout(() => alert("New Highscore!"), 500)
+        }
+        document.querySelector(".highscore").innerHTML = `Highscore on this level: ${keypressCount}`
+    }
+    ;
+}
 
